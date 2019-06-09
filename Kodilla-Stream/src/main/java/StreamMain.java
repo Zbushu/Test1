7e5c7c70942219ec0d9c9d9df9f96f5e42a45a30
Zbushu <l_oo_k@wp.pl>
2019-06-09 19:47:16 +0200

    7.2.0

--- Kodilla-Stream/src/main/java/StreamMain.java
+++ Kodilla-Stream/src/main/java/StreamMain.java
@@ -1,5 +1,6 @@
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.FunctionalCalculator;
@@ -16,6 +17,9 @@ public class StreamMain {
         poemBeautifier.beautify("Poem3", String::toLowerCase);
         poemBeautifier.beautify("Poem4", (a)-> a.substring(2,a.length()-2));
         poemBeautifier.beautify("Poem5", (a)-> a.replace("m","s"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 
 }
